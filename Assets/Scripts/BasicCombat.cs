463666119e831c57d4b639cd0091572662c33f37
Antoine SARRAZIN <tony78.itouch@gmail.com>
2019-04-14 19:21:22 +0200

    Reorganizing assets and Combat tests
    - Put Sprites in Ressources Folder
    - Created a Scripts forder to put scripts
    - Duplicated SampleScene map to create a GD Test map
    - Created a dumb script to try and put combat input on a fake sword (to test swipe feel and how it should work... It is really not great)

---
+++ Assets/Scripts/BasicCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCombat : MonoBehaviour
+{
+    [SerializeField] private KeyCode c_attack;
+
+    private Animation c_swipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c_swipe = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(c_attack))
+        {
+            c_swipe.Play();
+        }
+
+    }
+}
