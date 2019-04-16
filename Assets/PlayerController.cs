64a5c085d3b69786c59ec9979286f791648b15d4
Clement-R <rondeau.clement@gmail.com>
2019-04-16 21:03:41 +0200

    + add dash to the player controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,6 +12,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float m_speed = 2f;
     [SerializeField] private bool m_debug = false;
 
+    [Header("Dash")]
+    [SerializeField] private Vector2 m_dashForce = new Vector2();
+    [SerializeField] private float m_dashCooldown = 0f;
+    [SerializeField] private float m_dashDuration = 0.25f;
+    [SerializeField] private KeyCode m_dashKey;
+    private bool m_isDashing;
+    private float m_lastDash = 0f;
+
     private Rigidbody2D m_rb2d;
     private Vector2 m_movement = new Vector2();
 
@@ -24,17 +32,26 @@ public class PlayerController : MonoBehaviour
     {
         m_movement = Vector2.zero;
 
-        if (Input.GetKey(m_upKey))
-            m_movement += new Vector2(0f, 1f);
-        else if (Input.GetKey(m_downKey))
-            m_movement += new Vector2(0f, -1f);
+        if(m_isDashing)
+        {
+            if (Input.GetKey(m_upKey))
+                m_movement += new Vector2(0f, 1f);
+            else if (Input.GetKey(m_downKey))
+                m_movement += new Vector2(0f, -1f);
 
-        if (Input.GetKey(m_leftKey))
-            m_movement += new Vector2(-1f, 0f);
-        else if (Input.GetKey(m_rightKey))
-            m_movement += new Vector2(1f, 0f);
+            if (Input.GetKey(m_leftKey))
+                m_movement += new Vector2(-1f, 0f);
+            else if (Input.GetKey(m_rightKey))
+                m_movement += new Vector2(1f, 0f);
+        }
 
-        if(m_debug)
+        if (Input.GetKey(m_dashKey) && !m_isDashing && (Time.time >= m_lastDash + m_dashCooldown))
+        {
+            m_lastDash = Time.time;
+            m_isDashing = true;
+        }
+
+        if (m_debug)
         {
             Debug.DrawRay(transform.position, transform.right * 5f, Color.blue);
         }
@@ -43,10 +60,23 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Move(m_movement);
+
+        if (m_isDashing)
+        {
+            Dash();
+
+            if (Time.time >= m_lastDash + m_dashDuration)
+                m_isDashing = false;
+        }
     }
 
     private void Move(Vector2 p_movement)
     {
         m_rb2d.velocity = m_movement * m_speed;
+    }
+
+    private void Dash()
+    {
+        m_rb2d.AddForce(m_dashForce, ForceMode2D.Force);
     }
 }
